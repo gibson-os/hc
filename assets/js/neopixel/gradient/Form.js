4633e781b49a0ea9af8dd12f685939d38d2b3c99
Benjamin Wollenweber <ich@wollis-page.de>
2020-11-21 13:02:55 +0100

    Add gradient type

--- assets/js/neopixel/gradient/Form.js
+++ assets/js/neopixel/gradient/Form.js
@@ -9,6 +9,39 @@ Ext.define('GibsonOS.module.hc.neopixel.gradient.Form', {
         };
 
         me.items = [{
+            xtype: 'gosFormComboBox',
+            itemId: 'gosModuleHcNeopixelGradientType',
+            disabled: true,
+            fieldLabel: 'Verlaufstyp',
+            store: {
+                fields: [{
+                    name: 'id',
+                    type: 'int'
+                }, {
+                    name: 'name',
+                    type: 'string'
+                }],
+                data: [{
+                    id: 0,
+                    name: 'Nach Nummer aufsteigen'
+                },{
+                    id: 1,
+                    name: 'Nach Nummer absteigend'
+                },{
+                    id: 2,
+                    name: 'Von Links nach Rechts'
+                },{
+                    id: 3,
+                    name: 'Von oben nach unten'
+                },{
+                    id: 4,
+                    name: 'Von oben Links nach unten Rechts'
+                },{
+                    id: 5,
+                    name: 'Von oben Rechts nach unten Links'
+                }]
+            }
+        },{
             xtype: 'gosModuleHcNeopixelColorFadeIn'
         },{
             xtype: 'gosModuleHcNeopixelColorBlink'
