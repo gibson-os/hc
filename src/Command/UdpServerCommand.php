28b4b4a2fb9fa26abde035800c5abf614e52c224
Benjamin Wollenweber <ich@wollis-page.de>
2020-12-15 22:49:39 +0100

    Add flock to udp server

--- src/Command/UdpServerCommand.php
+++ src/Command/UdpServerCommand.php
@@ -6,8 +6,10 @@ namespace GibsonOS\Module\Hc\Command;
 use GibsonOS\Core\Command\AbstractCommand;
 use GibsonOS\Core\Exception\AbstractException;
 use GibsonOS\Core\Exception\ArgumentError;
+use GibsonOS\Core\Exception\Flock\LockError;
 use GibsonOS\Core\Exception\GetError;
 use GibsonOS\Core\Service\EnvService;
+use GibsonOS\Core\Service\LockService;
 use GibsonOS\Module\Hc\Service\Protocol\UdpService;
 use GibsonOS\Module\Hc\Service\ReceiverService;
 use mysqlDatabase;
@@ -15,6 +17,8 @@ use Psr\Log\LoggerInterface;
 
 class UdpServerCommand extends AbstractCommand
 {
+    private const LOCK_NAME = 'hcUdpServer';
+
     /**
      * @var UdpService
      */
@@ -35,17 +39,24 @@ class UdpServerCommand extends AbstractCommand
      */
     private $mysqlDatabase;
 
+    /**
+     * @var LockService
+     */
+    private $lockService;
+
     public function __construct(
         UdpService $protocol,
         ReceiverService $receiverService,
         EnvService $envService,
         mysqlDatabase $mysqlDatabase,
+        LockService $lockService,
         LoggerInterface $logger
     ) {
         $this->protocol = $protocol;
         $this->receiverService = $receiverService;
         $this->envService = $envService;
         $this->mysqlDatabase = $mysqlDatabase;
+        $this->lockService = $lockService;
 
         parent::__construct($logger);
 
@@ -58,6 +69,14 @@ class UdpServerCommand extends AbstractCommand
      */
     protected function run(): int
     {
+        try {
+            $this->lockService->lock(self::LOCK_NAME);
+        } catch (LockError $e) {
+            $this->logger->info('Server already runs!');
+
+            return 1;
+        }
+
         $this->protocol->setIp($this->getArgument('bindIp') ?? '0');
         $this->logger->info('Start server...');
 
