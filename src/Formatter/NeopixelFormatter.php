e83c0dcfb70880d9329f971c690ffff0e8feefef
Benjamin Wollenweber <ich@wollis-page.de>
2021-02-01 00:21:12 +0100

    Add get leds to neopixel formatter

--- src/Formatter/NeopixelFormatter.php
+++ src/Formatter/NeopixelFormatter.php
@@ -5,29 +5,39 @@ namespace GibsonOS\Module\Hc\Formatter;
 
 use GibsonOS\Module\Hc\Mapper\NeopixelMapper;
 use GibsonOS\Module\Hc\Model\Log;
-use GibsonOS\Module\Hc\Service\Slave\NeopixelService;
 use GibsonOS\Module\Hc\Service\TransformService;
+use GibsonOS\Module\Hc\Store\Neopixel\LedStore;
 
 class NeopixelFormatter extends AbstractHcFormatter
 {
-    private NeopixelService $neopixelService;
+    private LedStore $ledStore;
 
     private NeopixelMapper $neopixelMapper;
 
+    private array $leds = [];
+
     public function __construct(
         TransformService $transform,
-        NeopixelService $neopixelService,
+        LedStore $ledStore,
         NeopixelMapper $neopixelMapper
     ) {
         parent::__construct($transform);
-        $this->neopixelService = $neopixelService;
+        $this->ledStore = $ledStore;
         $this->neopixelMapper = $neopixelMapper;
     }
 
     public function render(Log $log): ?string
     {
-        $slave = $log->getModule();
-
         return parent::render($log);
+    }
+
+    private function getLeds(int $moduleId): array
+    {
+        if (!isset($this->leds[$moduleId])) {
+            $this->ledStore->setModule($moduleId);
+            $this->leds[$moduleId] = $this->ledStore->getList();
+        }
+
+        return $this->leds[$moduleId];
     }
 }
