39a14d3a474f6f51c51d1ad7ea10f772ab7f8395
Benjamin Wollenweber <ich@wollis-page.de>
2021-02-02 16:16:09 +0100

    Add copy log data to raw command

---
+++ src/Command/CopyLogDataToRawCommand.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace GibsonOS\Module\Hc\Command;
+
+use GibsonOS\Core\Command\AbstractCommand;
+use GibsonOS\Module\Hc\Model\Log;
+use GibsonOS\Module\Hc\Service\TransformService;
+use mysqlDatabase;
+use mysqlTable;
+use Psr\Log\LoggerInterface;
+
+class CopyLogDataToRawCommand extends AbstractCommand
+{
+    private mysqlDatabase $mysqlDatabase;
+
+    private TransformService $transformService;
+
+    public function __construct(LoggerInterface $logger, mysqlDatabase $mysqlDatabase, TransformService $transformService)
+    {
+        parent::__construct($logger);
+        $this->mysqlDatabase = $mysqlDatabase;
+        $this->transformService = $transformService;
+    }
+
+    protected function run(): int
+    {
+        $logTable = new mysqlTable($this->mysqlDatabase, Log::getTableName());
+        $logTable->setWhere('`data`!="" AND `raw_data`=""');
+
+        if (!$logTable->select()) {
+            return 1;
+        }
+
+        do {
+            $log = new Log($this->mysqlDatabase);
+            $log->loadFromMysqlTable($logTable);
+            $log->setRawData($this->transformService->hexToAscii($log->getData()));
+            $this->logger->info(sprintf(
+                'Transform #%d data from "%s" to "%s"',
+                $log->getId() ?? 0,
+                $log->getData(),
+                $log->getRawData()
+            ));
+            $log->save();
+        } while ($logTable->next());
+
+        return 0;
+    }
+}
