a2ed311ba992576d63755516ad6e7bbca6278915
Benjamin Wollenweber <ich@wollis-page.de>
2022-01-24 23:19:29 +0100

    Update ir protocols

--- src/Install/Data/IrProtocolData.php
+++ src/Install/Data/IrProtocolData.php
@@ -16,15 +16,15 @@ class IrProtocolData extends AbstractInstall implements PriorityInterface
     {
         $this->setSetting(
             'hc',
-            'ethbridgeIrProtocols',
+            'irProtocols',
             JsonUtility::encode([
                 1 => 'Sony', 2 => 'NEC', 3 => 'Samsung', 4 => 'Matsushita', 5 => 'Kaseikyo',
                 6 => 'Philips', 7 => 'RC5', 8 => 'Denon', 9 => 'RC6', 10 => 'Samsung32',
                 11 => 'Apple', 12 => 'Philips', 13 => 'Nubert', 14 => 'Bang & Olufsen',
                 15 => 'Grundig', 16 => 'Nokia', 17 => 'Siemens', 18 => 'FDC', 19 => 'RC Car',
-                20 => 'JVC', 21 => 'RC6A', 22 => 'Nikon', 23 => 'Ruwido', 24 => 'IR60', 25 => 'Kathrein',
-                26 => 'Netbox', 27 => 'NEC 16bit', 28 => 'NEC 42bit', 29 => 'LEGO', 30 => 'Thomson',
-                31 => 'BOSE', 32 => 'A1',
+                20 => 'JVC', 21 => 'RC6A', 22 => 'Nikon', 23 => 'Ruwido', 24 => 'IR60', 27 => 'NEC 16bit',
+                28 => 'NEC 42bit', 30 => 'Thomson', 31 => 'BOSE', 32 => 'A1', 34 => 'Telefunken',
+                35 => 'iRobot', 39 => 'Speaker', 40 => 'LG', 43 => 'Pentax', 49 => 'Mitsubishi', 52 => 'IRMP',
             ])
         );
 
