cc3231a24d5b49b366c9808e725012b0288ec9b4
Benjamin Wollenweber <ich@wollis-page.de>
2022-06-11 13:30:49 +0200

    Remove unsused dtos

--- src/Dto/Io/DirectConnect.php
+++ src/Dto/Io/DirectConnect.php
@@ -3,17 +3,10 @@ declare(strict_types=1);
 
 namespace GibsonOS\Module\Hc\Dto\Io;
 
-use GibsonOS\Module\Hc\Dto\Io\DirectConnect\Command;
 use GibsonOS\Module\Hc\Model\Io\DirectConnect as DirectConnectModel;
-use GibsonOS\Module\Hc\Model\Module;
 
 class DirectConnect
 {
-    /**
-     * @param Module    $module
-     * @param Port      $port
-     * @param Command[] $commands
-     */
     public function __construct(
         private readonly DirectConnectModel $directConnect,
         private readonly bool $hasMore,
