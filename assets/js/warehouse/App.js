470a994dcf93a26254f72f188dcc74ff1d592f2d
Benjamin Wollenweber <ich@wollis-page.de>
2022-06-23 15:09:25 +0200

    Add warehouse box store gui files

---
+++ assets/js/warehouse/App.js
@@ -0,0 +1,35 @@
+Ext.define('GibsonOS.module.hc.warehouse.App', {
+    extend: 'GibsonOS.module.hc.hcSlave.App',
+    alias: ['widget.gosModuleHcWarehouseApp'],
+    title: 'Warenhaus',
+    appIcon: 'icon_led',
+    width: 900,
+    height: 850,
+    requiredPermission: {
+        module: 'hc',
+        task: 'warehouse'
+    },
+    initComponent: function () {
+        let me = this;
+
+        me.items = [{
+            requiredPermission: {
+                module: 'hc',
+                task: 'neopixel'
+            },
+            xtype: 'gosModuleHcWarehouseBoxPanel',
+            title: 'Boxen',
+            hcModuleId: me.gos.data.module.id
+        },{
+            requiredPermission: {
+                module: 'hc',
+                task: 'neopixel'
+            },
+            xtype: 'gosModuleHcNeopixelLedPanel',
+            title: 'LEDs',
+            hcModuleId: me.gos.data.module.id
+        }];
+
+        me.callParent();
+    }
+});
---
+++ assets/js/warehouse/box/Panel.js
@@ -0,0 +1,12 @@
+Ext.define('GibsonOS.module.hc.warehouse.box.Panel', {
+    extend: 'GibsonOS.module.core.component.Panel',
+    alias: ['widget.gosModuleHcWarehouseBoxPanel'],
+    layout: 'border',
+    enableContextMenu: true,
+    enableKeyEvents: true,
+    initComponent() {
+        const me = this;
+
+        me.callParent();
+    }
+});
